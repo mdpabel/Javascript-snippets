5b856d6484921dbd9bdf982e1c7c6526a786cff8
mdpabel <64641522+mdpabel@users.noreply.github.com>
2023-01-24 20:42:31 +0600

    1 file modified

--- index.js
+++ index.js
@@ -2,45 +2,56 @@ class Node {
   constructor(val) {
     this.val = val;
     this.next = null;
+    this.prev = null;
   }
 }
 
-class SinglyLinkedList {
+class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
   }
 
-  addToTail(value) {
-    let newNode = new Node(value);
-    if (this.tail) this.tail.next = newNode;
-    else this.head = newNode;
+  addToTail(val) {
+    let newNode = new Node(val);
+    if (this.tail) {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    } else {
+      this.head = newNode;
+    }
     this.tail = newNode;
   }
 
+  addToHead(val) {
+    let newNode = new Node(val);
+    if (this.head) {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+    } else {
+      this.tail = newNode;
+    }
+    this.head = newNode;
+  }
+
   removeTail() {
-    let node = this.head;
-    if (!node) {
+    if (!this.tail) {
       return;
     }
-
-    if (node && !node.next) {
+    if (this.head === this.tail) {
       this.head = null;
       this.tail = null;
       return;
     }
 
-    while (node && node.next && node.next.next) {
-      node = node.next;
-    }
-    node.next = null;
-    this.tail = node;
+    this.tail = this.tail.prev;
+    this.tail.next = null;
   }
 }
 
-const ll = new SinglyLinkedList();
+const ll = new DoublyLinkedList();
 ll.addToTail(2);
-
-ll.removeTail();
+ll.addToHead(3);
+ll.addToHead(4);
 
 console.log(ll);
